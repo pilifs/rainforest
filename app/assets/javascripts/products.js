5e5d7ef127e0852195469b35ecebbf8b072f1699
Filip Stankovic <filips@gmail.com>
2016-02-17 15:39:43 -0500

    Add comments

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -5,12 +5,14 @@ $(document).on('ready page:load', function() {
 
     $.getScript('/products?search=' + searchValue);
   });
-
-   $(window).scroll(function() {
-      var url = $('.pagination span.next').children().attr('href');
-      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-        $('.pagination').text("Fetching more products...");
-        return $.getScript(url);
-      }
-   });
 });
+
+// Below does not need to be in doc.ready otherwise the scroll does not work after receiving an AJAX response. It also only works when there is a scroll bar (ie: if you search for something without a scroll bar, then search again, you can't scroll)
+
+$(window).scroll(function() {
+    var url = $('.pagination span.next').children().attr('href');
+    if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+      $('.pagination').text("Fetching more products...");
+      return $.getScript(url);
+    }
+ });
