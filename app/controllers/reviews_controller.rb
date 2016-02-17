16f37aa4c2bc10dd5b7bef9d5449a27944c9a1c9
Filip Stankovic <filips@gmail.com>
2016-02-17 16:21:29 -0500

    Add ajax form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,11 +11,15 @@ before_action :load_product, :ensure_logged_in, only: [:create, :destroy]
     @review = @product.reviews.build(review_params)
     @review.user = current_user
 
-    if @review.save
-      redirect_to products_path, notice: 'Review created successfully'
-    else
-      render 'products/show'
-    end
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to product_path(@product.id), notice: 'Review added.' }
+        format.js {} # This will look for app/views/reviews/create.js.erb
+      else
+        format.html { render 'products/show', alert: 'There was an error.'  }
+        format.js {} # This will look for app/views/reviews/create.js.erb
+      end
+    end    
   end
 
   def destroy
